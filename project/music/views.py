2666967fece4176c629b7282f027710ddf33961d
notXenonForce <xenonforce004@gmail.com>
2025-01-10 20:43:28 +0800

    fixed import and admin

--- project/music/views.py
+++ project/music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, indirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
 # Create your views here.
